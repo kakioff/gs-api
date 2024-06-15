82e3692c492e988feb4eccf10436112b1e116276
byron <1636700244@qq.com>
2024-06-15 14:40:49 +0800

    init

---
+++ src/main.py
@@ -0,0 +1,41 @@
+import logging
+import time
+from fastapi import FastAPI, Response
+from fastapi.requests import Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
+# region 初始化数据库
+import database
+
+database.Base.metadata.create_all(bind=database.engine)
+# endregion
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response: Response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
+
+from router import router
+
+app.include_router(router)
